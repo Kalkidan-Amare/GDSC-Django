d0b995c4ba5fe5efd9181c855778f80465b0b48e
Kalkidan Amare <kalkidanamare11a@gmail.com>
2023-11-28 19:31:38 +0300

    README and student_db.py

---
+++ week2/student_db.py
@@ -0,0 +1,73 @@
+class Student:
+    def __init__(self):
+        self.students = {}
+
+    def add_student(self,name,age,grade):
+        self.students[name] ={'age':age, 'grade':grade}
+    
+    def view_student(self,name):
+        if name in self.students:
+            student=self.students[name]
+            print(f"\nName {name}\nAge {student['age']}\nGrade {student['grade']}\n")
+        else:
+            print(f"Student {name} not found!\n")
+
+    def list_all(self):
+        for name, student in self.students.items():
+            print(f"\nName {name}\nAge {student['age']}\nGrade {student['grade']}\n")
+
+    def update_student(self,name,age=None,grade=None):
+        if name in self.students:
+            student=self.students[name]
+            if (age != None):
+                student['age'] = age
+            if (grade != None):
+                student['grade'] = grade
+            else:
+                print(f"Student {name} not found!\n")
+
+    def delete_student(self,name):
+        if name in self.students:
+            del self.students[name]
+
+def main():
+    stud=Student()
+
+    while(True):
+        print("\n ===============================")
+        print("|    Student database Menu      |")
+        print(" ===============================\n")
+        print("\t1. Add Student")
+        print("\t2. View Student")
+        print("\t3. List All Students")
+        print("\t4. Updeate Student Information")
+        print("\t5. Delete Student")
+        print("\t0. Exit\n")
+
+        choice=input("Enter your choice: ")
+
+        if choice=='1':
+            name=input("Enter the student's name: ")
+            age=input("Enter the age: ")
+            grade=input("Enter the grade: ")
+            stud.add_student(name,age,grade)
+        elif choice=='2':
+            name=input("Enter the student's name to search: ")
+            stud.view_student(name)
+        elif choice=='3':
+            stud.list_all()
+        elif choice=='4':
+            name=input("Enter the name of the student to be changed: ")
+            age=input("Enter the age: ")
+            grade=input("Enter the grade: ")
+            stud.update_student(name,age,grade)
+        elif choice=='5':
+            name=input("Enter the name of the student to be deleted: ")
+            stud.delete_student(name)
+        elif choice=='0':
+            break
+        else:
+            print("Please enter a valid choice")
+
+if __name__=='__main__':
+    main()
